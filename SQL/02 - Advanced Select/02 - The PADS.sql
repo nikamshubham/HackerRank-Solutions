a5d6955fd88d810b803a2333765d62c18b250d03
SHUBHAM NIKAM <65148970+nikamshubham@users.noreply.github.com>
2021-02-04 00:49:39 +0530

    Update 02 - The PADS.sql

--- SQL/02 - Advanced Select/02 - The PADS.sql
+++ SQL/02 - Advanced Select/02 - The PADS.sql
@@ -18,7 +18,7 @@ ORDER BY NAME ASC;
 SELECT CONCAT("There are a total of ", COUNT(Occupation), ' ', LOWER(Occupation), "s.")
 FROM OCCUPATIONS
 GROUP BY Occupation
-ORDER BY COUNT(Occupation), Occupation ASC;
+ORDER BY COUNT(Occupation) ASC;
 
 -- ========================
 --       Explanation
